1d44eb93343cfe0cfafd5b24986e4c3860d9b00a
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-21 14:01:46 +0330

    test and lunche current dashboard page

--- analitics/descriptive/dashboard.py
+++ analitics/descriptive/dashboard.py
@@ -6,4 +6,11 @@ from flask import (
 from analitics.descriptive.core.config import settings
 
 app = Flask(settings.APP_NAME)
-print(app)
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
