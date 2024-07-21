47ef26cfcd013410167a6b4dc94e9a2bcb8e3526
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-21 14:53:40 +0330

    add method to add url rule app flask

--- analitics/dashboard.py
+++ analitics/dashboard.py
@@ -5,8 +5,8 @@ from analitics.core.config import settings
 
 app = Flask(settings.APP_NAME)
 
-app.add_url_rule("/", view_func=views.home)
-app.add_url_rule("/dashboard", view_func=views.dashboard)
+app.add_url_rule("/", methods=["GET"], view_func=views.home)
+app.add_url_rule("/dashboard", methods=["GET"], view_func=views.dashboard)
 
 if __name__ == "__main__":
     print(f"loading analitic website {app.name} ...")
