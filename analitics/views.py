901aa7bff95057f4bc74b495285faa4e2af7957b
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-22 16:10:39 +0330

    parsed unique variable from view file to dashboard page

--- analitics/views.py
+++ analitics/views.py
@@ -7,10 +7,18 @@ def home():
 
 
 def dashboard():
+    unique_user = 0
+    unique_movie = 0
+    unique_tag = 0
+    unique_ratings = 0
     return render_template(
         "dashboard.html",
         all_user=record_users,
         all_ratings=record_ratings,
         all_movies=record_movies,
         all_tags=record_tags,
+        unique_user=unique_user,
+        unique_movie=unique_movie,
+        unique_tag=unique_tag,
+        unique_ratings=unique_ratings,
     )
